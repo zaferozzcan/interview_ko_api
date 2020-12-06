6804af82a3d25d24962ed54647a7521672beb8e9
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-06 14:50:22 -0500

    add params

--- routers/subjectRouter.js
+++ routers/subjectRouter.js
@@ -4,6 +4,16 @@ const Subject = require("../models/subjectModel");
 
 const subjectRouter = express.Router();
 
+subjectRouter.get("/:id", (req, res) => {
+  Subject.findOne({ userId: req.params.id }, (err, data) => {
+    if (!err) {
+      res.status(200).json(data);
+    } else {
+      res.status(400).json({ error: err.message });
+    }
+  });
+});
+
 subjectRouter.post("/new/", (req, res) => {
   console.log("new subj body", req.body);
   Subject.create(req.body, (err, data) => {
